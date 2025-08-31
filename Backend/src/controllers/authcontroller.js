5f4202f6ab9b4d68fb7e663d6c7e719ec86d0e44
Hamsavardhini B <hamsavardhinibaskar@gmail.com>
2025-08-31 21:01:48 +0530

    authentication and authorization modification

--- Backend/src/controllers/authcontroller.js
+++ Backend/src/controllers/authcontroller.js
@@ -1,8 +1,8 @@
 import User from '../models/User.js';
-import {generateToken} from '../utils/generateToken.js';
+import { generateToken } from '../utils/generateToken.js';
 
 // Register User
-export const registerUser = async (req, res,next) => {
+export const registerUser = async (req, res, next) => {
     try {
         const { name, email, password } = req.body;
         const userExists = await User.findOne({ email });
@@ -11,12 +11,19 @@ export const registerUser = async (req, res,next) => {
             throw new Error('User already exists');
         }
         const user = await User.create({ name, email, password });
-        const payload={
-            id:user._id,
-            name:user.name,
-            email:user.email
-        }
-        generateToken(res, payload);
+        const payload = {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+        };
+        const token=generateToken(payload);
+        res.cookie('jwt',token,{
+        httpOnly:true,
+        secure:false,
+        sameSite:"Lax",
+        path:"/",
+        maxAge:3600*2000
+    })
         res.status(201).json({
             _id: user.id,
             name: user.name,
@@ -35,45 +42,51 @@ export const loginUser = async (req, res, next) => {
 
         if (!user) {
             res.status(401);
-            throw new Error("Invalid email or password");
+            throw new Error('Invalid email or password');
         }
 
         const isMatch = await user.matchPassword(password);
 
         if (!isMatch) {
             res.status(401);
-            throw new Error("Invalid email or password");
+            throw new Error('Invalid email or password');
         }
 
         const payload = {
             id: user._id,
             name: user.name,
-            email: user.email
+            email: user.email,
         };
 
-        generateToken(res, payload);
-
+        const token=generateToken(payload);
+        res.cookie('jwt',token,{
+        httpOnly:true,
+        secure:false,
+        sameSite:"Lax",
+        path:"/",
+        maxAge:3600*2000
+        })
         res.json({
             name: user.name,
             email: user.email,
-            message:"User Logined successfully"
+            message: 'User Logined successfully',
         });
     } catch (err) {
         next(err);
     }
 };
 
-
 // logout
-export const logoutUser=(req,res,next)=>{
-    try{
-        res.cookie("jwt","",{
-            httpOnly:true,
-            expires:new Date(0),
-        })
-        return res.status(200).json({ message: "Logged out successfully" });
-    }
-    catch(err){
+export const logoutUser = (req, res, next) => {
+    try {
+        res.clearCookie('jwt', {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'Lax',
+            path: '/',
+        });
+        return res.status(200).json({ message: 'Logged out successfully' });
+    } catch (err) {
         next(err);
     }
-}
+};
--- Backend/src/utils/generateToken.js
+++ Backend/src/utils/generateToken.js
@@ -1,15 +1,8 @@
 import { env } from "../config/env.js";
 import jwt from "jsonwebtoken"
 
-export const generateToken=(res,payload)=>{
-    const token=jwt.sign(payload,env.JWT_SECRET,{
+export const generateToken=(payload)=>{
+   return jwt.sign(payload,env.JWT_SECRET,{
         expiresIn:"2h",
-    })
-    res.cookie('jwt',token,{
-        httpOnly:true,
-        secure:false,
-        SameSite:"Lax",
-        path:"/",
-        maxAge:3600*2000
     })
 }
